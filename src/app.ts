c6c571049e9ca9596ecd387a19050f226bd71e35
DIMPLE <Ddimple@rrc.ca>
2025-11-10 18:02:06 -0600

    Feat: created the main application file for our endpoints to define.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Express } from "express";
+import studentRoutes from "./api/v1/routes/Student_routes";
+import CourseRoutes from "./api/v1/routes/Course_routes";
+import EnrollmentRoutes from "./api/v1/routes/Enrollment_routes";
 
-// Initialize Express application
 const app: Express = express();
+app.use(express.json());
 
 // Define a route
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+app.use("/api/students", studentRoutes);
+app.use("/api/courses", CourseRoutes);
+app.use("/api/enrollments", EnrollmentRoutes)
 
 export default app;
